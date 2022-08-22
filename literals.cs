8878064a6fe750cb5b28bfb44ef683b192b68b87
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-08-23 00:46:20 +0530

    Create literals.cs

---
+++ literals.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2_Literals_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            float y = 4.5f;
+            Console.WriteLine("The result is {0}", x*y);
+            Console.Read();
+        }
+    }
+}
